0190ebf627f4c4e698f7172b99d3f2109fc0b162
CGibney <cbgibney@gmail.com>
2022-09-26 11:01:50 -0700

    first commit

---
+++ src/test/java/com/goldenoaks/main/GoldenOaksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goldenoaks.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenOaksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
