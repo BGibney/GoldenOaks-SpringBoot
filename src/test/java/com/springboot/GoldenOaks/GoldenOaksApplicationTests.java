d83c0ffa7e47242e14c4bcf900df9d6b71b7a1a0
bgibney <cbgibney@gmail.com>
2022-09-27 15:21:54 -0700

    initial commit to add extra dependencies

---
+++ src/test/java/com/springboot/GoldenOaks/GoldenOaksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.GoldenOaks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenOaksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
